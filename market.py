fa3d5bafb939148f5ba1dbbdcab5f53138a2d02b
Mohitchvk <mohit.chvk@gmail.com>
2025-02-24 12:27:50 -0500

    update logo

--- market.py
+++ market.py
@@ -48,7 +48,7 @@ st.set_page_config(
 
 logo = Image.open("passagetoindia2.png")  # Ensure "logo.png" is in your working directory
 st.image(logo, use_column_width=True)
-st.markdown("<div style='text-align: center;'><img src='logo.png' width='150'></div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center;'><img src='passagetoindia2.png' width='15'></div>", unsafe_allow_html=True)
 
 
 # Initialize session state
